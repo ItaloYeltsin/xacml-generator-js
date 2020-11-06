cebcbf3de9bf3f02f7c422ce182349f97aff7bb1
Italo Bruno <br.yeltsin@gmail.com>
2020-11-06 13:53:09 -0300

    components: added prototypes of missing components

---
+++ src/xacml/AttributeAssignment.ts
@@ -0,0 +1,10 @@
+// Copyright (C) 2020 Italo Yeltsin Medeiros Bruno
+// Copyright (c) 2020 Paulo Jonathas Alves Matos
+//
+// SPDX-License-Identifier: MIT
+
+import { XACMLElement } from "./XACMLElement";
+
+export class AttributeAssignment extends XACMLElement {
+  
+}
---
+++ src/xacml/AttributeValue.ts
@@ -0,0 +1,10 @@
+// Copyright (C) 2020 Italo Yeltsin Medeiros Bruno
+// Copyright (c) 2020 Paulo Jonathas Alves Matos
+//
+// SPDX-License-Identifier: MIT
+
+import { Expression } from "./Expression";
+
+export class AttributeValue extends Expression {
+  
+}
