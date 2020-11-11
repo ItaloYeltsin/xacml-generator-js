1157a7d9ccbfcf74b9fa3d32863e88a017911889
Italo Bruno <br.yeltsin@gmail.com>
2020-11-11 12:03:52 -0300

    xacml: implemented Element PolicySet

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,26 +1,60 @@
-import {expect} from 'chai';
+import {expect, use} from 'chai';
+import 'chai-subset'
 import { AnyOf } from '../xacml/AnyOf';
 import { Policy } from '../xacml/Policy';
 import { PolicyIdReference } from '../xacml/PolicyIdReference';
 import { PolicySet } from '../xacml/PolicySet';
 import { Target } from '../xacml/Target';
 import { XACMLElement } from '../xacml/XACMLElement';
-
-
+import {PolicyIssuer} from '../xacml/PolicyIssuer'
+import chaiSubset = require('chai-subset');
+import { Description } from '../xacml/Description';
+use(chaiSubset)
 describe('Element classes suite', function() {
     
     it('XACMLElement instances should have elementName equal class name', function() {
-        expect(new Policy("description").elementName).equal('Policy');
-        expect(new PolicySet("description").elementName).equal('PolicySet');
+        let policySet = new PolicySet({
+            description: new Description("This is a policy set"),
+            target: new Target(),
+            policyCombiningAlgId: 
+            'urn:oasis:names:tc:xacml:1.0:policy-combining-algorithm:deny-overrides',
+            version: '1.0',
+            policySetId: 'urn:oasis:names:tc:xacml:3.0:example:policysetid:1'
+        });
+        expect(policySet.elementName).equal('PolicySet');
         expect(new AnyOf().elementName).equal('AnyOf');
         expect(new Target().elementName).equal('Target');
     });
     
-    it('XACMLTopLevelElement should auto convert multi-valued fields to array', function () {
+    it('XACMLElement should auto add array elements fields as children', function () {
         let policyIdRef = new PolicyIdReference();
-        let policySet = new PolicySet('desc');
-        policySet.addChild(policyIdRef);
-        policySet.addChild(policyIdRef)
-        expect(policySet.elementRoot.PolicySet.PolicyIdReference.length).to.be.equal(2);    
+        let policySet = new PolicySet({
+            description: new Description("This is a policy set"),
+            target: new Target(),
+            policyCombiningAlgId: 
+            'urn:oasis:names:tc:xacml:1.0:policy-combining-algorithm:deny-overrides',
+            version: '1.0',
+            policySetId: 'urn:oasis:names:tc:xacml:3.0:example:policysetid:1',
+            policyIdReferences: [
+                new PolicyIdReference(),
+                new PolicyIdReference()
+            ] 
+        })
+        expect(policySet.elementRoot.PolicyIdReference.length).to.be.equal(2);    
+    });
+
+    it('Expect elements to have the same attributes of their interfaces', function() {
+        let policysetOptions: PolicySet.Options = {
+            description: new Description("This is a policy set"),
+            policyIssuer: new PolicyIssuer(),
+            target: new Target(),
+            policyCombiningAlgId: 
+            'urn:oasis:names:tc:xacml:1.0:policy-combining-algorithm:deny-overrides',
+            version: '1.0',
+            policySetId: 'urn:oasis:names:tc:xacml:3.0:example:policysetid:1'
+
+        }
+        let policySet = new PolicySet(policysetOptions)
+        expect(policySet).to.containSubset(policysetOptions)
     });
 });
